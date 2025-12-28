531695815409864b635504ed31c0781c689899bc
Enotii <anesnovpi21v@gmail.com>
2025-12-28 12:23:55 +0300

    Создание таблицы и добавление роутера

--- app/main.py
+++ app/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+from database import engine
+from notes.models import Base
+from routers import notes
 
+
+Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
+app.include_router(notes.router)
 
 @app.get("/")
 async def root():
