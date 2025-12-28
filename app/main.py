a612a85ff3b51264d199e9862c5c10cbd0aa3f4a
Enotii <anesnovpi21v@gmail.com>
2025-12-28 19:17:52 +0300

    Убран стандартный тестовый эндпоинт

--- app/main.py
+++ app/main.py
@@ -13,7 +13,3 @@ app.include_router(notes.router)
 async def root():
     return {"message": "Hello World"}
 
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
