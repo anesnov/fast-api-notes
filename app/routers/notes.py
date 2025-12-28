f15052c3af5abb1b99f54284d21c481736b5d718
Enotii <anesnovpi21v@gmail.com>
2025-12-28 18:31:15 +0300

    Добавление фильтров при получении списка заметок

--- app/routers/notes.py
+++ app/routers/notes.py
@@ -1,8 +1,10 @@
+from typing import List, Optional
+
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from database import get_db
 from notes.models import NoteDB
-from notes.schemas import NoteCreate, Note
+from notes.schemas import NoteUpdate, NoteCreate, Note
 from datetime import date
 
 
@@ -25,3 +27,27 @@ def create_source(data: NoteCreate, db: Session = Depends(get_db)):
 def get_note(id: int, db: Session = Depends(get_db)):
     note = db.query(NoteDB).where(NoteDB.id == id).first()
     return note
+
+@router.get("/", response_model=List[Note],
+            description="Получение списка всех заметок")
+def get_all_notes(
+        db: Session = Depends(get_db),
+        owner: Optional[str] = None,
+        search: Optional[str] = None,
+        date_from: Optional[date] = None,
+        date_to: Optional[date] = None):
+
+    query = db.query(NoteDB)
+
+    filters = [
+        NoteDB.owner == owner if owner else None,
+        ((NoteDB.title.ilike(f"%{search}%")) | (NoteDB.text.ilike(f"%{search}%"))) if search else None,
+        NoteDB.created_at >= date_from if date_from else None,
+        NoteDB.created_at <= date_to if date_to else None,
+    ]
+
+    for f in filters:
+        if f is not None:
+            query = query.filter(f)
+
+    return query.all()
